768c07fdb9829e3f20cc32fbd800614a2d591ab0
Esteban <edjeordjian@fi.uba.ar>
2022-06-07 22:08:45 -0300

    Checking environment for edge case

--- services/checkHostExists.py
+++ services/checkHostExists.py
@@ -1,11 +1,14 @@
 import logging
 from infrastructure.db.database import current_connection
 import logging
+
+from utils.utils import is_production
+
 logging.basicConfig(level=logging.DEBUG)
 
 
 def checkHostExists(destinyHost):
-    if "localhost" in destinyHost:
+    if "localhost" in destinyHost and not is_production():
         return True
 
     response = current_connection.find_one(
