5762e778caa9cb550497e90a35aa4053c8710a05
lionelguglielmone <lionel@guglielmone.com>
2022-06-26 20:37:04 -0300

    tests

--- tests/test_get_services.py
+++ tests/test_get_services.py
@@ -1,7 +1,8 @@
 import pytest
 
-from services.apikey_auth import _getServices, getServices, _enableApiKey
-from unittest.mock import patch
+from services.apikey_auth import _getServices, getServices, _enableApiKey, enableApiKey
+from unittest.mock import patch, MagicMock, AsyncMock, Mock
+import asyncio
 
 # pytest only detects functions or classes that to start with "test"
 # and are contained in a file with a name starting with "test".
@@ -17,7 +18,7 @@ dummy_value = 'patched dummy value'
 
 @patch("services.apikey_auth.apikey_found_and_active", return_value=True)
 @patch("services.apikey_auth.getAvailableServicesFromDB", return_value=dummy_value)
-def test_get_services_success(*args):
+def test_01_get_services_success(*args):
     response = _getServices("request")
 
     # The structure is actual = expected, where expected is a hardcode
@@ -30,13 +31,13 @@ dummy_value_2 = "one of my dummy values"
 
 @pytest.mark.asyncio
 @patch("services.apikey_auth._getServices", return_value=dummy_value_2)
-async def test_get_services(*args):
+async def test_02_get_services(*args):
     response = await getServices("request", None)
 
     assert response == dummy_value_2
 
 @patch("services.apikey_auth.apikey_found_and_active", return_value=False)
-def test_get_services_cannot_access_due_to_unauthorized_users(*args):
+def test_03_get_services_cannot_access_due_to_unauthorized_users(*args):
     response = _getServices("request")
 
     # The structure is actual = expected, where expected is a hardcode
@@ -44,28 +45,67 @@ def test_get_services_cannot_access_due_to_unauthorized_users(*args):
 
 
 
-dummy_data = {'apiKey':202020}
+#dummy_data = {'apiKey':202020}
+
+dummy_data = {
+    "apiKey": 202020,
+    "name": "dummy",
+    "active": True,
+    "creationDate": 0,
+    "description": "dummy"
+}
 @patch("services.apikey_auth.apikey_found_and_active", return_value=False)
-def test_enableApiKey_cannot_access_due_to_unauthorized_users(*arg):
+def test_04_enableApiKey_cannot_access_due_to_unauthorized_users(*arg):
     response = _enableApiKey(dummy_data)
 
     assert response.status_code == 401
+    assert "No autorizado" in str(response.body)
+
 
 
 @patch("services.apikey_auth.apikey_found_and_active", return_value=True)
 @patch("services.apikey_auth.current_connection.find_one", return_value=dummy_value)
 @patch("services.apikey_auth.current_connection.update_one")
-
-def test_enableApiKey(*arg):
+def test_05_enableApiKeyExistingApikey(*arg):
     response = _enableApiKey(dummy_data)
 
     assert response.status_code == 200
+    assert "ok" in str(response.body)
 
 
+
+@patch("services.apikey_auth.apikey_found_and_active", return_value=True)
+@patch("services.apikey_auth.current_connection.find_one", return_value=None)
+@patch("services.apikey_auth.current_connection.insert_one")
+def test_06_enableApiKeyNonExistingApikey(*arg):
+    response = _enableApiKey(dummy_data)
+
+    assert response.status_code == 200
+    assert "ok" in str(response.body)
+
+#----------------------------------------------------------------
+
+@pytest.mark.asyncio
+@patch("services.apikey_auth._enableApiKey", return_value="ok")
+async def test_07_enableApiKey(request):
+
+    #mock = AsyncMock()
+    #mock.x.return_value = {'json': 2020}
+    #res = await mock.x()
+
+    #res = await request.json()
+    #request.return_value = Mock(status_code=201, json=lambda : {"data": {"id": "test"}})
+
+
+    response = await enableApiKey(request)
+
+    assert response.status_code == 200
+
+#----------------------------------------------------------------
 
 @pytest.mark.asyncio
 @patch("services.apikey_auth._getServices", return_value=dummy_value_2)
-async def test_enableServices(*args):
+async def test_02_get_services(*args):
     response = await getServices("request", None)
 
     assert response == dummy_value_2
